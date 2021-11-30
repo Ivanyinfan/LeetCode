e41ab084e1dbf7fdd2fb0c5c56841ab23ff3b07d
Fan Yin <FanY@lifealert.com>
2021-11-29 18:03:43 -0800

    127

--- 127.word-ladder.py
+++ 127.word-ladder.py
@@ -10,28 +10,56 @@ class Node:
     def __init__(self, word, intermediate) -> None:
         self.word = word
         self.intermediate = intermediate
+        self.level = 1
+
+    def __hash__(self) -> int:
+        return hash(self.word)
+    
+    def __eq__(self, __o: object) -> bool:
+        return self.word==__o.word
 
 class Solution:
     def addToGraph(self, word, graph):
         wordNode = Node(word, False)
+        neigh = graph[wordNode]
         for i in range(len(word)):
             tmp = Node(f'{word[:i]}*{word[i+1:]}', True)
-            graph[wordNode].append(tmp)
-            graph[tmp].append(word)
+            neigh.append(tmp)
+            graph[tmp].append(wordNode)
+        return wordNode
 
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         graph = defaultdict(list)
         for word in wordList:
             self.addToGraph(word, graph)
-        if endWord not in graph:
-            return 0
+        beginWord:Node = Node(beginWord, False)
         if beginWord not in graph:
-            self.addToGraph(beginWord, graph)
+            self.addToGraph(beginWord.word, graph)
         visited = {beginWord}
-        stack = deque()
+        stack = deque([beginWord])
         while stack:
             node:Node = stack.popleft()
-
+            if not node.intermediate and node.word==endWord:
+                return node.level//2+1
+            for neigh in graph[node]:
+                if neigh not in visited:
+                    neigh.level = node.level + 1
+                    stack.append(neigh)
+                    visited.add(neigh)
+        return 0
 
 # @lc code=end
+from utils import test
+if __name__ == "__main__":
+    sol = Solution()
+    test(sol.ladderLength("hit", "cog", ["hot","dot","dog","lot","log","cog"]), 5)
+    test(sol.ladderLength("hit", "cog", ["hot","dot","dog","lot","log"]), 0)
+    test(sol.ladderLength("hit", "cog", ["hot","dot","dog","lot","log","cog", 'hit']), 5)
+    test(sol.ladderLength("hit", "cog", ["cog"]), 0)
+    test(sol.ladderLength("hit", "cog", ["cog", 'hit']), 0)
+    test(sol.ladderLength("hit", "cog", []), 0)
 
+# Accepted
+# 49/49 cases passed (256 ms)
+# Your runtime beats 53.43 % of python3 submissions
+# Your memory usage beats 5.21 % of python3 submissions (25.7 MB)
