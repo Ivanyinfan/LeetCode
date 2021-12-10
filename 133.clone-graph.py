efa20e12bc6e79380f0cc5181e182abfec8dc8a2
Fan Yin <FanY@lifealert.com>
2021-12-09 19:38:42 -0800

    133

--- 133.clone-graph.py
+++ 133.clone-graph.py
@@ -20,26 +20,48 @@ class Node:
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node: return None
-        root = Node(node.val)
+        root = Node(node.val, [None]*len(node.neighbors))
         visited = {root.val: root}
         stack = [[node, 0]]
         while stack:
             if stack[-1][1]==len(stack[-1][0].neighbors):
                 stack.pop()
                 continue
-            for i in range(stack[-1][1], len(stack[-1][0].neighbors)):
-                val = stack[-1][0].neighbors[i].val
+            node: Node = stack[-1][0]
+            for i in range(stack[-1][1], len(node.neighbors)):
+                neighbour_i = node.neighbors[i]
+                val = neighbour_i.val
                 if val not in visited:
-                    copy = Node(val)
-                    visited[val]=copy
-                    stack[-1][1] += 1
-                    stack.append([stack[-1].neighbors[i],0])
-                    break
-                else:
-                    visited[stack[-1][0].val].neighbors[i] = visited[val]
+                    copy = Node(val, [None]*len(neighbour_i.neighbors))
+                    visited[val] = copy
                     stack[-1][1] = i+1
+                    visited[node.val].neighbors[i] = visited[val]
+                    stack.append([neighbour_i,0])
+                    break
+                visited[node.val].neighbors[i] = visited[val]
+                stack[-1][1] = i+1
         return root
 
 
 # @lc code=end
+from utils import test
+if __name__ == "__main__":
+    sol = Solution()
+    node_1 = Node(1)
+    node_2 = Node(2)
+    node_3 = Node(3)
+    node_4 = Node(4)
+    node_1.neighbors.append(node_2)
+    node_1.neighbors.append(node_4)
+    node_2.neighbors.append(node_1)
+    node_2.neighbors.append(node_3)
+    node_3.neighbors.append(node_2)
+    node_3.neighbors.append(node_4)
+    node_4.neighbors.append(node_3)
+    node_4.neighbors.append(node_1)
+    sol.cloneGraph(node_1)
 
+# Accepted
+# 22/22 cases passed (63 ms)
+# Your runtime beats 8.98 % of python3 submissions
+# Your memory usage beats 60.23 % of python3 submissions (14.6 MB)
