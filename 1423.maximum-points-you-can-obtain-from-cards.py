8da6ec8a563561f6f8f62d541877474ce7cba3a2
Fan Yin <FanY@lifealert.com>
2022-01-14 17:50:47 -0800

    1423

--- 1423.maximum-points-you-can-obtain-from-cards.py
+++ 1423.maximum-points-you-can-obtain-from-cards.py
@@ -18,4 +18,17 @@ class Solution:
             result = max(prefixSum+suffixSum, result)
         return result
 # @lc code=end
+from utils import test
+if __name__ == "__main__":
+    sol = Solution()
+    test(sol.maxScore(cardPoints = [1,2,3,4,5,6,1], k = 3), 12)
+    test(sol.maxScore(cardPoints = [2,2,2], k = 2), 4)
+    test(sol.maxScore(cardPoints = [9,7,7,9,7,7,9], k = 7), 55)
+    test(sol.maxScore(cardPoints = [9], k = 1), 9)
+    test(sol.maxScore(cardPoints = [9,8], k = 1), 9)
+    test(sol.maxScore(cardPoints = [9,8], k = 2), 17)
 
+# Accepted
+# 40/40 cases passed (568 ms)
+# Your runtime beats 26.03 % of python3 submissions
+# Your memory usage beats 36.88 % of python3 submissions (27.7 MB)
