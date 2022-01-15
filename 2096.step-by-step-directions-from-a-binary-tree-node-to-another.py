10de5e62c47b01952a196c1e7415d6b444287354
Fan Yin <FanY@lifealert.com>
2022-01-14 17:54:49 -0800

    2096

--- 2096.step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096.step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -28,6 +28,7 @@ class Solution:
         if treeNode.right:
             fathers[treeNode.right]=treeNode
             self.dfs(treeNode.right, path, fathers, pathResult)
+        path.pop()
         
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
         fathers = dict()
@@ -36,23 +37,35 @@ class Solution:
         self.destValue = destValue
         self.dfs(root, list(), fathers, pathResult)
         l1,l2 = len(pathResult[0]), len(pathResult[1])
-        path2_set = set(pathResult[1])
+        # path1 = list(map(lambda x:x.val, pathResult[0]))
+        # path2 = list(map(lambda x:x.val, pathResult[1]))
+        path2_dict = dict()
+        for i, node in enumerate(pathResult[1]):
+            path2_dict[node]=i
         result = list()
         LCA = None
         for i in range(l1-1, -1, -1):
-            if pathResult[0][i] not in path2_set:
+            if pathResult[0][i] not in path2_dict:
                 result.append('U')
             else:
                 LCA = pathResult[0][i]
                 break
-        index = pathResult[1].find(LCA)
-        currNode = LCA
-        while currNode.val!=destValue:
-            if pathResult[1][index+1]==currNode.left:
+        index = path2_dict[LCA]+1
+        while index!=l2:
+            if pathResult[1][index]==pathResult[1][index-1].left:
                 result.append('L')
             else:
                 result.append('R')
+            index+=1
         return ''.join(result)
         
-# @lc code=end  
+# @lc code=end
+from utils import test, list2TreeNode
+if __name__ == "__main__":
+    sol = Solution()  
+    test(sol.getDirections(root = list2TreeNode([5,1,2,3,None,6,4]), startValue = 3, destValue = 6), "UURL")
+    test(sol.getDirections(root = list2TreeNode([2,1]), startValue = 2, destValue = 1), "L")
 
+# Success 
+# Runtime: 1108 ms, faster than 45.71% of Python3 online submissions for Step-By-Step Directions From a Binary Tree Node to Another.
+# Memory Usage: 134.8 MB, less than 57.32% of Python3 online submissions for Step-By-Step Directions From a Binary Tree Node to Another.
