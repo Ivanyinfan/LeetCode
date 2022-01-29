2fd882071d0fff669fe2301456a87f5e256fb6b2
Fan Yin <FanY@lifealert.com>
2022-01-28 19:59:27 -0800

    249

--- 249.Group Shifted Strings.py
+++ 249.Group Shifted Strings.py
@@ -13,7 +13,10 @@ class Solution:
             tmp = [None]*len(string)
             base = ord(string[0])
             for i, char in enumerate(string):
-                tmp[i] = ord(char) - base
+                distance = ord(char) - base
+                if distance<0:
+                    distance += 26
+                tmp[i] = distance
             resultMap[tuple(tmp)].append(string)
         return resultMap.values()
 
@@ -23,3 +26,8 @@ if __name__ == "__main__":
     sol = Solution()
     test(sol.groupStrings(strings = ["abc","bcd","acef","xyz","az","ba","a","z"]), [["acef"],["a","z"],["abc","bcd","xyz"],["az","ba"]], ordered=False)
     test(sol.groupStrings(strings = ["a"]), [['a']], ordered=False)
+
+# Success
+# Details 
+# Runtime: 69 ms, faster than 11.92% of Python3 online submissions for Group Shifted Strings.
+# Memory Usage: 13.9 MB, less than 99.96% of Python3 online submissions for Group Shifted Strings.
